25caa201beb7b02afdae476c05244e3742e44388
rav <rav@codevery.com>
2019-07-05 16:52:48 +0300

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Socialite;
+use App\User;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        $user = Socialite::driver($provider)->user();
+        $authUser = $this->findOrCreateUser($user, $provider);
+        Auth::login($authUser, true);
+        return redirect($this->redirectTo);
+    }
+    private function findOrCreateUser($user, $provider){
+        $authUser = User::where('email', $user->email)->first();
+        return $authUser ? $authUser :
+            User::create([
+                'name' => $user->getName(),
+                'email' => $user->getEmail(),
+                'password' => Hash::make(Str::random(10)),
+                'avatar' => $user->getAvatar(),
+                'provider' => $provider,
+                'provider_id' => $user->getId(),
+            ]);
+
+    }
+}
---
+++ resources/views/welcome-mail.blade.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>New user account created!</title>
+    <!--[if gte mso 9]>
+    <style>
+        h1 {
+            padding: 0 !important;
+            margin-top: 0 !important;
+            margin-bottom: 0 !important;
+        }
+
+        h2 {
+            padding: 0 !important;
+            margin-top: 5px !important;
+            margin-bottom: 5px !important;
+        }
+
+        p {
+            padding: 0 !important;
+            margin-bottom: 20px !important;
+        }
+
+        ul {
+            padding: 0 !important;
+            margin-left: 30px !important;
+        }
+
+        .mso-email-button {
+            background-color: #ffffff !important;
+            color: #0000ff !important;
+            text-decoration: underline !important;
+            text-align: left !important;
+            padding-left: 0 !important;
+            margin-bottom: 40px !important;
+        }
+
+        .mso-email-cta {
+            display: inline !important;
+        }
+
+        #email-right-column {
+            margin-right: 20px !important;
+            margin-bottom: 0 !important;
+        }
+
+        #email-footer-accent {
+            padding: 0 !important;
+            margin-top: 10px !important;
+            margin-bottom: 10px !important;
+        }
+
+        #email-footer-container {
+            padding: 0 !important;
+        }
+
+        #email-utility-header {
+            padding: 0 !important;
+            margin-top: 5px !important;
+            margin-bottom: 5px !important;
+        }
+
+        #mso-email-p-cta {
+            margin-bottom: 0 !important;
+        }
+
+        .email-table-spacer {
+            display: none !important;
+        }
+
+        .email-footer {
+            margin-top: 10px !important;
+            margin-bottom: 10px !important;
+        }
+
+        .email-footer-link {
+            color: #222222 !important;
+        }
+    </style>
+    <![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style type="text/css">
+        html {
+        }
+
+        /* Outlook link fix */
+        #outlook a {
+            padding: 0;
+        }
+
+        /* Hotmail background \26  line height fixes */
+        .ExternalClass {
+            width: 100% !important;
+        }
+
+        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+            line-height: 100%;
+        }
+
+        /* Image borders \26  formatting */
+        img {
+            outline: none;
+            text-decoration: none;
+            -ms-interpolation-mode: bicubic;
+        }
+
+        a img {
+            border: none;
+        }
+
+        /*Re-style iPhone automatic links */
+        .applelinks a {
+            color: #222222;
+            text-decoration: none;
+        }
+
+        /* Hotmail symbol fix for mobile */
+        .ExternalClass img[class^=Emoji] {
+            width: 10px !important;
+            height: 10px !important;
+            display: inline !important;
+        }
+
+        @media screen and (min-width: 641px) {
+
+        }
+
+        @media screen and (max-width: 640px) {
+            [class=email-full-width] {
+                width: 100% !important;
+                height: auto !important;
+            }
+
+            [class=email-image] {
+                width: 100% !important;
+                height: auto !important;
+            }
+
+            [class=email-full-width-squeeze] {
+                width: 100% !important;
+            }
+
+            [class=email-col-split-table] {
+                float: left !important;
+                width: 100% !important;
+                padding: 10px 0 !important;
+            }
+
+            [class=no-mobile] {
+                display: none !important;
+            }
+
+            [class=email-ap-thumb] {
+                width: 60% !important;
+                height: auto !important;
+            }
+
+            [class=email-col-split-image] {
+                float: left !important;
+                width: 100% !important;
+                padding: 0 !important;
+            }
+
+            [class=email-col-split-right] {
+                float: left !important;
+                width: 90% !important;
+                text-align: center !important;
+                padding: 10px 20px !important;
+            }
+
+            [class=email-col-split-left] {
+                float: left !important;
+                width: auto !important;
+                padding: 10px 20px !important;
+            }
+
+            [class=email-p-mobile] {
+                font-size: 16px !important;
+                line-height: 28px !important;
+            }
+
+            [class=email-h1-mobile] {
+                text-align: left !important;
+                font-size: 22px !important;
+                line-height: 1.2 !important;
+            }
+
+            [id=call-to-action-mobile] {
+                display: block !important;
+                width: 100% !important;
+                padding-left: 0 !important;
+                padding-right: 0 !important;
+                text-align: center;
+            }
+
+            [id=email-utility-header] {
+                display: none !important;
+            }
+
+            [class=email-title1] {
+                font-size: 29px !important;
+                line-height: 34px !important;
+            }
+
+            [class=email-title2] {
+                font-size: 32px !important;
+                line-height: 38px !important;
+            }
+
+            [class=email-footer] {
+                margin-left: 20px !important;
+                margin-right: 20px !important;
+                padding-bottom: 5px !important;
+            }
+
+            [class=mobile-signature-image] {
+                margin-left: -5px !important;
+                width: 100% !important;
+            }
+
+            [class=mobile-signature-content] {
+                width: 100% !important;
+                padding: 0 !important;
+            }
+
+            [class=email-sig-p] {
+                line-height: 16px !important;
+                margin-bottom: 8px !important;
+                font-size: 14px !important;
+                width: 100% !important;
+            }
+
+            [class=email-sig-name] {
+                line-height: 26px !important;
+                font-size: 14px !important;
+            }
+        }
+    </style>
+</head>
+<body
+    style="padding:0; margin:0; -webkit-text-size-adjust:none; -ms-text-size-adjust:100%; background-color:#f0f0f0; font-size:14px; font-family:Arial, Helvetica, sans-serif; color:#000000; line-height:24px;">
+<table border="0" cellspacing="0" cellpadding="0" width="100%" bgcolor="#f0f0f0" style="height:100%;">
+    <tr>
+        <td align="center" valign="top">
+
+            <!----------------------------------------------- UTILITY HEADER -->
+            <table width="600" border="0" cellspacing="0" cellpadding="0" bgcolor="#f0f0f0" class="email-full-width">
+                <tr>
+                    <td class="no-mobile"> </td>
+                </tr>
+            </table>
+
+            <!----------------------------------------------- MAIN PAGE CONTAINER -->
+            <table width="600" border="0" cellspacing="0" cellpadding="40" bgcolor="#ffffff" class="email-full-width">
+                <tr>
+                    <td align="center">
+
+
+                        <!----------------------------------------------- TITLE SECTION -->
+                        <table width="500" border="0" cellspacing="0" cellpadding="10" bgcolor="#ffffff"
+                               class="email-full-width">
+                            <tr>
+                                <td style="padding-left:40px;padding-right:40px;text-align:center;">
+                                </td>
+                            </tr>
+                        </table>
+                        <!----------------------------------------------- MAIN CONTENT SECTION -->
+                        <table width="500" border="0" cellspacing="0" cellpadding="10" bgcolor="#ffffff"
+                               class="email-full-width">
+                            <tr>
+                                <td style="padding-left:20px;padding-right:20px;text-align:left;">
+
+                                    <p style="font-size:18px;line-height:21px;padding:0;margin:0;margin-bottom:20px;font-family:Arial, Helvetica, sans-serif;color:#444444;"
+                                       class="email-p-mobile">Hello!</p>
+
+                                    <p style="font-size:14px;line-height:21px;padding:0;margin:0;margin-bottom:40px;font-family:Arial, Helvetica, sans-serif;color:#444444;"
+                                       class="email-p-mobile">A new user account has been created for you on Daily
+                                        Report Card Online (DRCO). Please click the link below to create a new password
+                                        for your account.</p>
+
+                                    <p style="font-size:14px;line-height:19px;padding:0;margin:0;margin-bottom:20px;font-family:Arial, Helvetica, sans-serif;color:#444444;text-align:center;"
+                                       class="email-p-mobile">
+                                    </p>
+
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!-- FOOTER -->
+            <table width="600" border="0" cellspacing="0" cellpadding="0" bgcolor="#f0f0f0" class="email-full-width">
+                <tr>
+                    <td align="center">
+
+                        <!----------------------------------------------- SPAM ACT STUFF -->
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#f0f0f0"
+                               class="email-full-width">
+                            <tr>
+                                <td align="center">
+                                    <p style="font-size:11px;line-height:18px;padding:0;margin:0;padding-top:10px;padding-bottom:0px;font-family:Arial, Helvetica, sans-serif;color:#999999;"
+                                       class="email-footer">© 2019 Center for Intervention Research in Schools, Ohio
+                                        University</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td height="10"></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
